43172e2800edec0db041cb52620e1ccd28e7a095
jyotijoey <jburnwal5@gmail.com>
2021-05-26 18:07:21 +0530

    added delete functionality

--- app.js
+++ app.js
@@ -25,7 +25,32 @@ class LinkedList{
         }
     }
 
-    
+    delete(value){
+        if(!this.head){
+            console.log("the list is empty");
+            return;
+        }
+
+        while (this.head && this.head.value === value){
+            this.head=this.head.next;
+        }
+
+        let curNode = this.head;
+
+        while(curNode.next){
+            if (curNode.next.value===value){
+                curNode.next =curNode.next.next; //updating the next property of the current node to the next node of the node getting deleted
+            }
+            else{
+                curNode= curNode.next; //updating the value of current node for the loop to work
+            }
+        }
+
+        if(this.tail.value===value){
+            this.tail = curNode;
+        }
+
+    }
 
     toArray() {
         const elements = [];
@@ -45,8 +70,16 @@ const LinkedList1 = new LinkedList();
 LinkedList1.append(1);
 LinkedList1.append("hello world");
 LinkedList1.append("jyoti");
+LinkedList1.append("jyoti");
+LinkedList1.append("jyoti");
 LinkedList1.append(true);
 LinkedList1.append(67.70);
-LinkedList1.prepend("i am first")
+LinkedList1.prepend("i am first");
+
+console.log(LinkedList1.toArray());
+
+LinkedList1.delete("i am first");
+LinkedList1.delete(67.70);
+LinkedList1.delete("jyoti");
 
 console.log(LinkedList1.toArray());
